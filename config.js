ece59795cab46f20858a33cd7df22f86eb683818
Steven <huangying0621@163.com>
2022-09-08 16:29:20 +0800

    新增软件升级功能

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.$config = {
   name: "动力环境监控系统",
-  version: "v1.2.3 build211025",
-  ip: "127.0.0.1",
-  port: 12880,
+  version: "v1.2.4 build220908",
+  ip: "192.168.10.110",
+  port: 12280,
   path: "api/bi/",
   user: "ht",
 };
