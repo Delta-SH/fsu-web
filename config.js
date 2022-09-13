be032f0dc0cf5752abc1047a2c7e3f4f8bb75573
Steven <huangying0621@163.com>
2022-09-13 11:32:31 +0800

    v1.2.5

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.$config = {
   name: "动力环境监控系统",
-  version: "v1.2.4 build220908",
-  ip: "192.168.10.110",
-  port: 12280,
+  version: "v1.2.5 build220913",
+  ip: "127.0.0.1",
+  port: 8080,
   path: "api/bi/",
   user: "ht",
 };
