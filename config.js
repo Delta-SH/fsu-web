d98e1584e501850ce8f6ec09acbe163a40162faf
Steven <huangying0621@163.com>
2023-11-05 12:49:37 +0800

    更新jquery、underscore、moment脚本版本

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.$config = {
   name: "动力环境监控系统",
-  version: "v1.2.7 build230328",
+  version: "v1.2.8 build231105",
   ip: "127.0.0.1",
-  port: 8080,
+  port: 8099,
   path: "api/bi/",
   user: "ht",
   picker: false,
